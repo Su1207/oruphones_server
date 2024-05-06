9e7c33f81dfd8ca02859e5b001353742a3293843
suraj maheshwari <surajmaheshwari159@gmail.com>
2024-05-07 00:11:50 +0530

    initial commit

--- dist/utils/db.js
+++ dist/utils/db.js
@@ -2,14 +2,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const connectDB = async () => {
-    try {
-        const uri = process.env.MONGODB_URI; // Replace with your connection string
-        await mongoose.connect(uri);
-        console.log("MongoDB connected successfully");
+  try {
+    const uri = process.env.MONGODB_URI; // Replace with your connection string
+    if (uri) {
+      await mongoose.connect(uri);
+      console.log("MongoDB connected successfully");
     }
-    catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1);
-    }
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
 };
 export default connectDB;
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,9 +4,11 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI; // Replace with your connection string
-    await mongoose.connect(uri);
-    console.log("MongoDB connected successfully");
+    const uri: string = process.env.MONGODB_URI; // Replace with your connection string
+    if (uri) {
+      await mongoose.connect(uri);
+      console.log("MongoDB connected successfully");
+    }
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
